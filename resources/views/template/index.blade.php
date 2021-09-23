79da790aa07683f846eff87fdb85764c133ac361
Pascal Lacroix <webiepscf@gmail.com>
2021-09-23 11:55:41 +0200

    Intégration du template

---
+++ resources/views/template/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('template.partials._head')
+  </head>
+  <body>
+
+	    @include('template.partials._nav')
+
+        @include('template.partials._header')
+
+        @include('template.partials._main')
+
+        @include('template.partials._footer')
+
+        @include('template.partials._loader')
+
+        @include('template.partials._scripts')
+  
+  </body>
+</html>
